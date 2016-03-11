d22c230ec14a45d87d3886d0c9e02b402fed3d80
soma_arc <somaarcr@gmail.com>
2016-03-11 15:06:53 +0900

    Add distance function of Kleinian group

--- src/ray/geometry.clj
+++ src/ray/geometry.clj
@@ -42,6 +42,102 @@
             [false isect]))
         [false isect]))))
 
+(def +sphere-pos1+ [5 5 0])
+(def +sphere-pos2+ [5 -5 0])
+(def +sphere-pos3+ [-5 5 0])
+(def +sphere-pos4+ [-5 -5 0])
+(def +sphere-pos5+ [0 0 7.071])
+(def +sphere-pos6+ [0 0 -0.071])
+(def +sphere-r+ 5)
+(def +sphere-r2+ 5)
+(def +max-klein-loop+ 3)
+(def klein-sphere-r 2.0833)
+
+(defn dist-func [center pos]
+  (loop [i 0
+         pos (v/+ pos center)
+         dr 1.]
+    (if (> i +max-klein-loop+)
+      [(* 0.08
+          (/ (- (v/length pos) klein-sphere-r)
+             (Math/abs dr))) (dec i)]
+      (cond
+        (< (v/length (v/- pos +sphere-pos1+)) +sphere-r+)
+        (let [diff (v/- pos +sphere-pos1+)
+              dr (* dr (/ +sphere-r2+ (v/dot diff diff)) )
+              pos (v/+ (v/scale (v/scale diff +sphere-r2+)
+                                (/ 1 (* (v/length diff) (v/length diff))))
+                       +sphere-pos1+)]
+          (recur (inc i) pos dr))
+        (< (v/length (v/- pos +sphere-pos2+)) +sphere-r+)
+        (let [diff (v/- pos +sphere-pos2+)
+              dr (* dr (/ +sphere-r2+ (v/dot diff diff)) )
+              pos (v/+ (v/scale (v/scale diff +sphere-r2+)
+                                (/ 1 (* (v/length diff) (v/length diff))))
+                       +sphere-pos2+)]
+          (recur (inc i) pos dr))
+        (< (v/length (v/- pos +sphere-pos3+)) +sphere-r+)
+        (let [diff (v/- pos +sphere-pos3+)
+              dr (* dr (/ +sphere-r2+ (v/dot diff diff)) )
+              pos (v/+ (v/scale (v/scale diff +sphere-r2+)
+                                (/ 1 (* (v/length diff) (v/length diff))))
+                       +sphere-pos3+)]
+          (recur (inc i) pos dr))
+        (< (v/length (v/- pos +sphere-pos4+)) +sphere-r+)
+        (let [diff (v/- pos +sphere-pos4+)
+              dr (* dr (/ +sphere-r2+ (v/dot diff diff)) )
+              pos (v/+ (v/scale (v/scale diff +sphere-r2+)
+                                (/ 1 (* (v/length diff) (v/length diff))))
+                       +sphere-pos4+)]
+          (recur (inc i) pos dr))
+        (< (v/length (v/- pos +sphere-pos5+)) +sphere-r+)
+        (let [diff (v/- pos +sphere-pos5+)
+              dr (* dr (/ +sphere-r2+ (v/dot diff diff)) )
+              pos (v/+ (v/scale (v/scale diff +sphere-r2+)
+                                (/ 1 (* (v/length diff) (v/length diff))))
+                       +sphere-pos5+)]
+          (recur (inc i) pos dr))
+        (< (v/length (v/- pos +sphere-pos6+)) +sphere-r+)
+        (let [diff (v/- pos +sphere-pos6+)
+              dr (* dr (/ +sphere-r2+ (v/dot diff diff)) )
+              pos (v/+ (v/scale (v/scale diff +sphere-r2+)
+                                (/ 1 (* (v/length diff) (v/length diff))))
+                       +sphere-pos6+)]
+          (recur (inc i) pos dr))
+        :default [(* 0.08
+                      (/ (- (v/length pos) klein-sphere-r)
+                         (Math/abs dr)))
+                  i]))))
+
+(defn get-normal [center p]
+  (v/normalize (v/->Vec (- (first (dist-func center (v/+ p (v/->Vec 0.01 0 0))))
+                           (first (dist-func center (v/- p (v/->Vec 0.01 0 0)))))
+                        (- (first (dist-func center (v/+ p (v/->Vec 0 0.01 0))))
+                           (first (dist-func center (v/- p (v/->Vec 0 0.01 0)))))
+                        (- (first (dist-func center (v/+ p (v/->Vec 0 0 0.01))))
+                           (first (dist-func center (v/- p (v/->Vec 0 0 0.01))))))))
+
+(def +max-marching-loop+ 100)
+(defrecord Klein [center material]
+  Intersectable
+  (intersect [this isect {ray-origin :origin ray-dir :dir :as ray}]
+    (loop [i 0
+           dist 0
+           ray-pos ray-origin
+           ray-length 0]
+      (if (>= i +max-marching-loop+)
+        [false isect]
+        (let [dist (first (dist-func center ray-pos))
+              ray-length (+ ray-length dist)
+              ray-pos (v/+ ray-origin (v/scale ray-dir ray-length))]
+          (if (< dist 0.001)
+            [true (assoc isect
+                         :t ray-length
+                         :p ray-pos
+                         :n (get-normal center ray-pos)
+                         :material material)]
+            (recur (inc i) dist ray-pos ray-length)))))))
+
 (defrecord Plane [n d material]
   Intersectable
   (intersect [this isect {ray-origin :origin ray-dir :dir :as ray}]
